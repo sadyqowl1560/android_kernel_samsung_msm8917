1b915980dd208a8c58115da82084c1ba18a03440
sadyqowl1560 <sadyqowl1560@gmail.com>
2020-12-23 08:07:40 -0500

    build : Increase build speed

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,6 +2,7 @@
 
 #SETUP BUILD ENVIROMENT
 #
+CORE_COUNT=`grep processor /proc/cpuinfo|wc -l`
 export ARCH=arm
 export PATH=/home/maos/tool-chain/arm-linux-androideabi-4.9/bin:$PATH
 
@@ -10,16 +11,15 @@ export PATH=/home/maos/tool-chain/arm-linux-androideabi-4.9/bin:$PATH
 OUTDIR=out
 rm -rf $OUTDIR
 mkdir $OUTDIR
-make clean  $OUTDIR && make mrproper $OUTDIR
+make clean -j$CORE_COUNT $OUTDIR && make mrproper -j$CORE_COUNT $OUTDIR
 
 #MAKE DEFCONFIG
 #
 CCV="CROSS_COMPILE=arm-linux-androideabi-"
 ODV="O=$OUTDIR"
-make -C $(pwd) $ODV $CCV j6primelte_swa_kali_open_defconfig
+make -j$CORE_COUNT -C $(pwd) $ODV $CCV j6primelte_swa_kali_open_defconfig
 
 #GET CPU COUNT AND BUILD
 #
-CORE_COUNT=`grep processor /proc/cpuinfo|wc -l`
 make -j$CORE_COUNT -C $(pwd) $ODV $CCV
 cp $OUTDIR/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
